3cabad33994aa98166163c16f8c2b66470d34b34
Bychuhan <xiechuhan2012x3@163.com>
2026-01-23 23:09:40 +0800

    feat: 纹理渲染

--- src/renderer.py
+++ src/renderer.py
@@ -4,6 +4,7 @@ import moderngl as mgl
 
 from config import *
 from shader import *
+from texture import *
 
 
 class Renderer:
@@ -15,6 +16,9 @@ class Renderer:
         # 初始化着色器
         self.shader_manager = ShaderManager()
         self._init_shaders()
+
+        # 初始化纹理
+        self.texture_manager = TextureManager()
 
     def set_blend(self, enable: bool = True):
         if enable:
@@ -30,6 +34,7 @@ class Renderer:
         self.ctx.clear(color=color)
 
     def _init_shaders(self):
+        # 渲染矩形着色器初始化
         with open(os.path.join(self.config.resources_dir, "shaders/rect/rect.vert")) as vert_file, open(os.path.join(self.config.resources_dir, "shaders/rect/rect.frag")) as frag_file:
             self.shader_manager.create_shader(
                 self.ctx,
@@ -55,6 +60,32 @@ class Renderer:
             (self.config.width, self.config.height)
         )
 
+        # 渲染纹理着色器初始化
+        with open(os.path.join(self.config.resources_dir, "shaders/texture/texture.vert")) as vert_file, open(os.path.join(self.config.resources_dir, "shaders/texture/texture.frag")) as frag_file:
+            self.shader_manager.create_shader(
+                self.ctx,
+                "texture",
+                [
+                    -1.0, -1.0, 0.0, 0.0,
+                    1.0, -1.0, 1.0, 0.0,
+                    1.0, 1.0, 1.0, 1.0,
+                    -1.0, 1.0, 0.0, 1.0
+                ],
+                vert_file.read(),
+                frag_file.read(),
+                in_types="2f 2f",
+                in_vars=["in_pos", "in_texCoord"],
+                indices=[
+                    0, 1, 2,
+                    0, 3, 2
+                ]
+            )
+
+        self.shader_manager.set_shader_uniform(
+            "texture", "screenSize",
+            (self.config.width, self.config.height)
+        )
+
     def render_rect(self, x: float, y: float, w: float, h: float,
                     r: float, color: list[float] | tuple[float] = (1, 1, 1, 1),
                     anchor: list[float] | tuple[float] = (0.5, 0.5)):
@@ -66,3 +97,20 @@ class Renderer:
         self.shader_manager.set_shader_uniform("rect", "color", color)
 
         self.shader_manager.use_shader("rect", mode=mgl.TRIANGLE_STRIP)
+
+    def render_texture(self, texture_name: str, x: float, y: float, sx: float, sy: float,
+                       r: float, color: list[float] | tuple[float] = (1, 1, 1, 1),
+                       anchor: list[float] | tuple[float] = (0.5, 0.5)):
+
+        self.shader_manager.set_shader_uniform("texture", "position", (x, y))
+        self.shader_manager.set_shader_uniform("texture", "scale", (sx, sy))
+        self.shader_manager.set_shader_uniform("texture", "textureSize",
+                                               self.texture_manager.get_texture_size(texture_name))
+        self.shader_manager.set_shader_uniform("texture", "anchor", anchor)
+        self.shader_manager.set_shader_uniform("texture", "rotation", r)
+        self.shader_manager.set_shader_uniform("texture", "color", color)
+
+        self.shader_manager.set_shader_uniform("texture", "texture", 0)
+        self.texture_manager.use_texture(texture_name, 0)
+
+        self.shader_manager.use_shader("texture", mode=mgl.TRIANGLE_STRIP)
