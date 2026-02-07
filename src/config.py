d04f17a318a89dbc51fa9a70be65a432ea68b903
Bychuhan <xiechuhan2012x3@163.com>
2026-02-07 13:57:48 +0800

    feat: 资源配置类

--- src/config.py
+++ src/config.py
@@ -16,3 +16,15 @@ class Config:
     encoder: str = "libx264"
     video_fps: int = 60
     video_bitrate: str = "15000k"
+
+
+@dataclass_json
+@dataclass
+class ResColors:
+    line_color: list[float]
+
+
+@dataclass_json
+@dataclass
+class ResConfig:
+    colors: ResColors
